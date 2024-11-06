b57f726fa5e2a3e89de637a5d1b66068eea9801d
Gabriel Rodriguez <gab.rodriguez11@outlook.com>
2024-11-06 15:58:24 -0700

    added pages and navbar templates

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar'
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p> */}
-        <p>
-          TOUR 409 Turkey Website
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React Documentation
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     {/* <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p> */}
+    //     <p>
+    //       TOUR 409 Turkey Website
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       React Documentation
+    //     </a>
+    //   </header
+
+    // main code
+    <div>
+      <div id='navHolder'>
+        <NavBar></NavBar>
+      </div>
+      <div id='pagePlaceholder'>
+        <Home></Home>
+      </div>
     </div>
   );
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    <div>
+      <h1>Navigation Bar (Create Components for Buttons)</h1>
+    </div>
+  );
+}
+
+export default NavBar;
