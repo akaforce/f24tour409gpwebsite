ad7cef2580a4ec38f9dd8a0d35fb4b34c5da1860
Gabriel Rodriguez <gab.rodriguez11@outlook.com>
2024-11-24 19:52:39 -0700

    fixed proportions (window display must be at 200% scale)

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,12 +9,12 @@ interface NavBarProps {
 const NavBar: React.FC<NavBarProps> = ({ setCurrentPage }) => {
   return (
     <div className='bg-gradient-to-r from-red-950 via-slate-950 to-red-950 flex flex-row  items-center justify-between px-[3%] py-9'>
-      <div id='logos_aisle' className='flex flex-row w-[45%]'>
-        <img src={fifaNavLogo} alt='fifa logo' className='h-36 w-auto'  onClick={() => setCurrentPage('Home')} />
-        <img src={goTurkiyeLogo} alt='go turkiye logo' className='h-36 w-auto' onClick={() => window.open('https://www.visitsturkiye.com/', '_blank')}/>
+      <div id='logos_aisle' className='flex flex-row w-[33%]'>
+        <img src={fifaNavLogo} alt='fifa logo' className='h-20 w-auto'  onClick={() => setCurrentPage('Home')} />
+        <img src={goTurkiyeLogo} alt='go turkiye logo' className='h-20 w-auto' onClick={() => window.open('https://www.visitsturkiye.com/', '_blank')}/>
       </div>
 
-      <div id='pages_nav' className='flex flex-row h-[30%] text-xl font-semibold gap-8'>
+      <div id='pages_nav' className='flex flex-row w-[64%] h-[30%] text-xl font-semibold gap-3'>
         <p className='cursor-pointer px-4 hover:text-red-300' onClick={() => setCurrentPage('Matches')}>Matches</p>
         <p className='cursor-pointer px-4 hover:text-red-300' onClick={() => setCurrentPage('Explore')}>Explore Türkiye</p>
         <p className='cursor-pointer px-4 hover:text-red-300' onClick={() => setCurrentPage('Packages')}>Packages</p>
